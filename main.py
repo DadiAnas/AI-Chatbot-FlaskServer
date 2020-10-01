b1e3491548e253ad5ac1e7a99912a0dac8f5cca1
Anas Dadi <dadi.anas@hotmail.fr>
2020-10-01 16:03:56 +0100

    init

---
+++ main.py
@@ -0,0 +1,23 @@
+from flask import Blueprint,jsonify
+from . import db,cursor
+
+main = Blueprint('main',__name__)
+
+
+@main.route('/')
+def hello_world():
+    #query = "CREATE TABLE intents (id INT NOT NULL PRIMARY KEY, name VARCHAR(40) )"
+    #cursor.execute(query)
+    #query = "INSERT INTO intents (id,name) VALUES ('{}','{}');".format(2,"anas")
+    #cursor.execute(query)
+    query = "ALTER TABLE intents DROP COLUMN Updated_at; "
+    cursor.execute(query)
+    db.commit()
+    query="SELECT * FROM intents"
+    cursor.execute(query)
+    print(cursor.column_names)
+    intents = cursor.fetchall()
+    response = dict()
+    for intent in intents:
+        response[intent[0]]=intent[1]
+    return jsonify({"response:":response})
