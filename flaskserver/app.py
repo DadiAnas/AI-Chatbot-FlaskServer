def08e50fa8d834705463817170c267d83daf400
Anas Dadi <dadi.anas@hotmail.fr>
2020-10-02 00:21:12 +0100

    deploy

---
+++ flaskserver/app.py
@@ -0,0 +1,15 @@
+from flask import Flask
+from flask_material import Material
+
+def create_app():
+    app = Flask(__name__, static_url_path='/static')
+    Material(app)
+    app.config['UPLOAD_FOLDER'] = '/static/styles/images/products'
+    app.config['MAX_CONTENT_PATH'] = '25,165,824'
+    app.config['SECRET_KEY'] = '9OLWxND4o83j4K4iuopO'
+
+    # blueprint for non-auth parts of app
+    from .main import main as main_blueprint
+    app.register_blueprint(main_blueprint)
+
+    return app
