9eee468710a50b73d2cacbed75ca1e533f9c1b78
Anas Dadi <40916777+DadiAnas@users.noreply.github.com>
2020-11-24 00:36:18 +0100

    Update app.py

--- app.py
+++ app.py
@@ -1,13 +1,11 @@
 from flask import Flask
-from flask_material import Material
 from project.main import main as main_blueprint
 
 
 app = Flask(__name__, static_url_path='/project/static/')
-Material(app)
+
 # blueprint for non-auth parts of app
 app.register_blueprint(main_blueprint)
-
 
 if __name__ == '__main__':
     app.run(threaded=True, port=5000)
