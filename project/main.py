051d61cea2dd1c679817ee95846357999bd3da35
Anas Dadi <40916777+DadiAnas@users.noreply.github.com>
2021-01-01 01:01:44 +0100

    Update main.py

--- project/main.py
+++ project/main.py
@@ -5,7 +5,11 @@ main = Blueprint('main',__name__)
 
 @main.route('/')
 def chatbot_home():
-    return "ok"
+    responses=["hi","hello","how are you ?","good to see you here"]
+    response = [
+        {'id':0 ,'message': 'Hello','trigger':1},
+                ]
+    return jsonify(response)
 
 @main.route('/message/', methods=['GET'])
 def respond():
