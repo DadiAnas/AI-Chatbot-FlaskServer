9ba72cfb3fc2928214a28617c3205b7cf0a6df87
Anas Dadi <dadi.anas@hotmail.fr>
2021-01-11 22:24:48 +0100

    working version

---
+++ project/main.py
@@ -0,0 +1,55 @@
+from flask import Blueprint,jsonify,request
+from .deepLearning.chatbot import chatbot_response
+
+main = Blueprint('main',__name__)
+
+@main.route('/')
+def chatbot_home():
+    responses=["hi","hello","how are you ?","good to see you here"]
+    response = [
+        {'id':0 ,'message': 'Hello','trigger':1},
+                ]
+    return jsonify(response)
+
+@main.route('/message/', methods=['GET'])
+def respond():
+    # Retrieve the name from url parameter
+    message = str(request.args.get("message", str))
+    chatbot_rep = chatbot_response(message)
+
+    # For debugging
+    print(f"got message {message}")
+    print(f"chatbot response: {chatbot_rep}")
+
+    response = {}
+
+    # Check if user sent a name at all
+    if not message:
+        response["ERROR"] = "no name found, please send a name."
+    # Check if the user entered a number not a name
+    elif str(message).isdigit():
+        response["ERROR"] = "name can't be numeric."
+    # Now the user entered a valid name
+    else:
+        response["MESSAGE"] = f"{message}"
+        response["MESSAGE"] = f"{chatbot_rep}"
+
+    # Return the response in json format
+    return jsonify(response)
+
+@main.route('/post/', methods=['POST'])
+def post_something():
+    name = request.form.get('name')
+    print(name)
+    # You can add the test cases you made in the previous function, but in our case here you are just testing the POST functionality
+    if name:
+        return jsonify({
+            "Message": f"Welcome {name} to our awesome platform!!",
+            # Add this option to distinct the POST request
+            "METHOD" : "POST"
+        })
+    else:
+        return jsonify({
+            "ERROR": "no name found, please send a name."
+        })
+
